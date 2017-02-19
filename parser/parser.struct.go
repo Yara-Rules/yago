f35311752b391b659d017e2217b484c19b1f52ce
Xumeiquer <jaumartin@gmail.com>
2017-02-19 20:54:01 +0100

    Fix #4 Added  key on json for Parse struct

--- parser/parser.struct.go
+++ parser/parser.struct.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Parser struct {
-	Name      string         `json:"-"       bson:"-"`
-	Lex       *lexer.Lexer   `json:"-"       bson:"-"`
-	LastItem  lexer.Item     `json:"-"       bson:"-"`
-	peekCount int            `json:"-"       bson:"-"`
-	token     [3]lexer.Item  `json:"-"       bson:"-"` // three-token lookahead for parser.
-	Imports   []string       `json:"imports" bson:"imports"`
-	Rules     []RuleDef      `json:"rules"   bson:"rules"`
-	log       *logrus.Logger `json:"-"       bson:"-"`
+	Name      string         `json:"file_name"  bson:"file_name"`
+	Lex       *lexer.Lexer   `json:"-"          bson:"-"`
+	LastItem  lexer.Item     `json:"-"          bson:"-"`
+	peekCount int            `json:"-"          bson:"-"`
+	token     [3]lexer.Item  `json:"-"          bson:"-"` // three-token lookahead for parser.
+	Imports   []string       `json:"imports"    bson:"imports"`
+	Rules     []RuleDef      `json:"rules"      bson:"rules"`
+	log       *logrus.Logger `json:"-"          bson:"-"`
 }
 
 type StringDef struct {
